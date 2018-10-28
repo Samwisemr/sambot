5dade9ad7679de39def63508ccd1c99f7b61b45e
Sam Meyer-Reed <samwisemr@gmail.com>
2018-10-27 22:37:33 -0400

    respond with nani

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ if __name__ == '__main__':
 
 
     @client.event
-    async def onMessage(message):
+    async def on_message(message):
         # ignore all bots
         if message.author.bot: return
+        if message.content.startswith('sambot ') or message.content.startswith('sambot, '): return
+
+        if message.content.lower() == 'omae wa mou shindeiru':
+            newMessage = 'NANI??'
+            await client.send_message(message.channel, newMessage)
 
 
 
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
 
 
     client.run(TOKEN)
-
 
