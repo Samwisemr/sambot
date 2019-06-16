ad462c096060dfa4d85d31080479c360c549cb85
Sam Meyer-Reed <samwisemr@gmail.com>
2019-06-16 01:31:57 -0400

    add error checking for reading auth.json

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import json
+import os
 
 from sambot import Sambot
 
+AUTH_FILE = 'auth.json'
+
 if __name__ == '__main__':
-    with open('auth.json') as jsonFile:
-        data = json.load(jsonFile)
-        token = data['token']
+    if os.path.exists(AUTH_FILE):
+        with open(AUTH_FILE) as jsonFile:
+            try:
+                data = json.load(jsonFile)
+                token = data['token']
+            except:
+                print('Error reading file: ' + AUTH_FILE + '. Exiting.')
+    else:
+        print('File ' + AUTH_FILE + ' could not be found. Exiting.')
 
     sambot = Sambot(token)
     sambot.run()
-
